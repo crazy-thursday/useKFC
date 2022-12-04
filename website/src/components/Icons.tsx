2a3ad44bcf492f22cf2bb61335c0b096a225ca51
innocces <18435105624@163.com>
2022-12-04 16:36:23 +0800

    style(dark mode): approve dark mode

---
+++ website/src/components/Icons.tsx
@@ -0,0 +1,28 @@
+/** @jsxImportSource react */
+import type { SVGProps } from 'react'
+
+export function Sun(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="#888888"
+        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="M12 19c.83 0 1.5-.67 1.5-1.5h-3c0 .83.67 1.5 1.5 1.5zm-3-4h6v1.5H9zm3-10c-2.76 0-5 2.24-5 5c0 1.64.8 3.09 2.03 4h5.95A4.985 4.985 0 0 0 17 10c0-2.76-2.24-5-5-5zm2.43 7.5H9.57A3.473 3.473 0 0 1 8.5 10c0-1.93 1.57-3.5 3.5-3.5s3.5 1.57 3.5 3.5c0 .95-.39 1.84-1.07 2.5z"
+      ></path>
+    </svg>
+  )
+}
+
+export function Moon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="#888888"
+        d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 17c-.83 0-1.5-.67-1.5-1.5h3c0 .83-.67 1.5-1.5 1.5zm2.25-2.5h-4.5c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h4.5c.41 0 .75.34.75.75s-.34.75-.75.75zm.72-2.5H9.03A4.966 4.966 0 0 1 7 10c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.64-.8 3.09-2.03 4z"
+      ></path>
+    </svg>
+  )
+}
---
+++ website/src/components/Theme.tsx
@@ -0,0 +1,52 @@
+import type { FC } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import { Sun, Moon } from './Icons'
+
+import 'src/styles/index.less'
+
+const ThemeToggle: FC = () => {
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
+    }
+    if (typeof localStorage !== undefined && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme')
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    }
+    return 'light'
+  })
+
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'light') {
+      root.removeAttribute('data-mode')
+    } else {
+      root.setAttribute('data-mode', 'dark')
+    }
+  }, [theme])
+
+  const handleToggleTheme = useCallback(() => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light'
+    localStorage.setItem('theme', nextTheme)
+    setTheme(nextTheme)
+  }, [theme])
+
+  return (
+    <button
+      className="relative p-0 m-0 rounded-xl w-10 h-6 border border-solid border-neutral-400 dark:border-neutral-600 dark:bg-neutral-700 transition-colors"
+      role="switch"
+      type="button"
+      aria-label={`toggle ${theme ?? 'dark'} mode`}
+      onClick={handleToggleTheme}
+    >
+      <span className="relative block w-full h-full dark:translate-x-2/4 transition-all">
+        <Sun className="absolute top-2/4 -translate-y-2/4 left-px sun text-lg" />
+        <Moon className="absolute top-2/4 -translate-y-2/4 left-px moon text-lg" />
+      </span>
+    </button>
+  )
+}
+
+export default ThemeToggle
