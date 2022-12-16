56e7bbbb0364e390fd6ee8c966787f38216475c6
innocces <18435105624@163.com>
2022-12-16 10:38:18 +0800

    feat(markdown): support markdown & fix theme error

--- website/src/components/KFC.tsx
+++ website/src/components/KFC.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource react */
-import { FC, useEffect, useState, useMemo, useCallback, useRef } from 'react'
-import useKFC, { useParseSlogen } from '@crazy-thursday/use-kfc'
+import { FC, useEffect, useState, useMemo, useCallback, useRef } from 'react';
+import useKFC, { useParseSlogen } from '@crazy-thursday/use-kfc';
 import {
   ChakraProvider,
   Button,
@@ -12,36 +12,39 @@ import {
   AlertDialogContent,
   AlertDialogFooter,
   AlertDialogOverlay,
-  useToast
-} from '@chakra-ui/react'
-import Copy from 'react-copy-to-clipboard'
+  useToast,
+  extendTheme
+} from '@chakra-ui/react';
+import Copy from 'react-copy-to-clipboard';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 // @ts-ignore
-import slogenList from '@crazy-thursday/use-kfc/slogen'
+import slogenList from '@crazy-thursday/use-kfc/slogen';
 
 const KFC: FC = () => {
-  const toast = useToast()
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const leastDestructiveRef = useRef()
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const leastDestructiveRef = useRef();
   const [themeMode, changeThemeMode] = useState<string>(
-    localStorage.getItem('theme')
-  )
-  const [refreshSignal, setRefreshSignal] = useState<number>(Date.now())
-  const list = useParseSlogen(slogenList)
+    localStorage.getItem('theme') ?? 'system'
+  );
+  const [refreshSignal, setRefreshSignal] = useState<number>(Date.now());
+  const list = useParseSlogen(slogenList);
   const { slogen } = useKFC<number>({
     slogenList: list,
     skipDayCheck: true,
     refreshSignal
-  })
+  });
 
   useEffect(() => {
     window.addEventListener('theme-change', (e) =>
       changeThemeMode((e as CustomEvent).detail as string)
-    )
-  }, [])
+    );
+  }, []);
 
   const showSlogenModal = useCallback(() => {
-    onOpen()
-  }, [slogen])
+    onOpen();
+  }, [slogen]);
 
   const handleCopy = useCallback(() => {
     toast({
@@ -49,16 +52,21 @@ const KFC: FC = () => {
       title: '复制成功! ',
       duration: 500,
       position: 'top'
-    })
-  }, [])
+    });
+  }, []);
 
   const ModeWrapper = useMemo(() => {
-    return themeMode === 'dark' ? DarkMode : LightMode
-  }, [themeMode])
+    return themeMode === 'dark' ? DarkMode : LightMode;
+  }, [themeMode]);
 
   return (
     <div className="absolute z-10 top-24 md:top-56 left-1/2 -translate-x-1/2 container min-h-max max-h-[35vh] text-center">
-      <ChakraProvider>
+      <ChakraProvider
+        theme={extendTheme({
+          initialColorMode: themeMode,
+          useSystemColorMode: false
+        })}
+      >
         <ModeWrapper>
           <Button colorScheme="purple" onClick={() => showSlogenModal()}>
             随机生成 KFC Slogen
@@ -67,15 +75,17 @@ const KFC: FC = () => {
             isOpen={isOpen}
             leastDestructiveRef={leastDestructiveRef}
             onClose={() => {
-              onClose()
-              setRefreshSignal(Date.now())
+              onClose();
+              setRefreshSignal(Date.now());
             }}
           >
             <AlertDialogOverlay />
             <AlertDialogContent h={300}>
               <AlertDialogBody pt={4} maxH={60} overflow="auto">
                 <pre className="whitespace-pre-wrap break-words text-[#242424]/90 dark:text-white/90">
-                  {slogen}
+                  <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                    {slogen}
+                  </ReactMarkdown>
                 </pre>
               </AlertDialogBody>
               <AlertDialogFooter>
@@ -100,7 +110,7 @@ const KFC: FC = () => {
         </ModeWrapper>
       </ChakraProvider>
     </div>
-  )
-}
+  );
+};
 
-export default KFC
+export default KFC;
