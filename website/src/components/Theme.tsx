a15facb164af2de0f256335c3f1ca248346eeba8
innocces <18435105624@163.com>
2022-12-16 10:38:18 +0800

    feat(markdown): support markdown & fix theme error

--- website/src/components/Theme.tsx
+++ website/src/components/Theme.tsx
@@ -1,38 +1,44 @@
-import type { FC } from 'react'
-import { useState, useEffect, useCallback } from 'react'
-import { Sun, Moon } from './Icons'
+import type { FC } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { Sun, Moon } from './Icons';
 
-import 'src/styles/index.less'
+import 'src/styles/index.less';
 
 const ThemeToggle: FC = () => {
   const [theme, setTheme] = useState(() => {
     if (import.meta.env.SSR) {
-      return undefined
+      return undefined;
     }
     if (typeof localStorage !== undefined && localStorage.getItem('theme')) {
-      return localStorage.getItem('theme')
+      return localStorage.getItem('theme');
     }
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark'
+      return 'dark';
     }
-    return 'light'
-  })
+    return 'light';
+  });
 
   useEffect(() => {
-    const root = document.documentElement
+    const root = document.documentElement;
     if (theme === 'light') {
-      root.removeAttribute('data-mode')
+      root.setAttribute('data-mode', 'light');
+      root.setAttribute('data-theme', 'light');
+      root.style.colorScheme = 'light';
     } else {
-      root.setAttribute('data-mode', 'dark')
+      root.setAttribute('data-mode', 'dark');
+      root.setAttribute('data-theme', 'dark');
+      root.style.colorScheme = 'dark';
     }
-  }, [theme])
+  }, [theme]);
 
   const handleToggleTheme = useCallback(() => {
-    const nextTheme = theme === 'light' ? 'dark' : 'light'
-    localStorage.setItem('theme', nextTheme)
-    window.dispatchEvent(new CustomEvent('theme-change', { detail: nextTheme }))
-    setTheme(nextTheme)
-  }, [theme])
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    localStorage.setItem('theme', nextTheme);
+    window.dispatchEvent(
+      new CustomEvent('theme-change', { detail: nextTheme })
+    );
+    setTheme(nextTheme);
+  }, [theme]);
 
   return (
     <button
@@ -47,7 +53,7 @@ const ThemeToggle: FC = () => {
         <Moon className="absolute top-2/4 -translate-y-2/4 left-px moon text-lg" />
       </span>
     </button>
-  )
-}
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
